a8dfecd4ea87eed1e03a04277295e8e412309690
Somendra Tiwari <49708374+somendrat22@users.noreply.github.com>
2026-03-09 00:57:16 +0530

    Clean up InputOutput.java and improve comments
    Removed commented-out code and added comments for clarity.

---
+++ Module-1/InputOutput.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Main {
+  public static void main(String[] args) {
+
+    // 
+    //   Scanner scn = new Scanner(System.in);
+    //   int x = scn.nextInt();
+    //   int y = scn.nextInt();
+    //   int z = scn.nextInt();
+    // long b = 10;
+    // int a = b;
+    // System.out.println(a);
+    
+    byte a = (byte)125; //
+    
+    byte c = (byte)10; //
+    
+    byte b = a + (byte)c;
+    
+    System.out.println(b);
+     
+    
+  }
+}
