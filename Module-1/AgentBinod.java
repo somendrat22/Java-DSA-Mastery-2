2f9d92e4842770e176be04034e6940a6e04ca2db
Somendra Tiwari <49708374+somendrat22@users.noreply.github.com>
2026-03-09 00:58:38 +0530

    Add AgentBinod.java with greeting functionality

---
+++ Module-1/AgentBinod.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String agentName = scn.nextLine();
+        String res = "Hi my name is Agent " + agentName;
+        System.out.println(res);
+    }
+}
