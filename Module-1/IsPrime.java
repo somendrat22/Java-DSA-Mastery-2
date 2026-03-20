50a80744e20592253e041d46ce46ebed27158ba8
Somendra Tiwari <49708374+somendrat22@users.noreply.github.com>
2026-03-20 23:24:30 +0530

    Create IsPrime.java

---
+++ Module-1/IsPrime.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        boolean isPrime = true;
+        for(int i = 2; i < n; i++){
+            if(n%i == 0){
+                isPrime = false;
+                break;
+            }
+        }
+
+        if(isPrime){
+            System.out.println(n + " is a prime number");
+        }else{
+            System.out.println(n + " is not a prime number");
+        }
+
+
+    }
+}
